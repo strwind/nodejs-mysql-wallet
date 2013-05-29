f25ad3b907cc178ae1cc77a727b8c35d56fb28aa
strwind <gzzyyff@gmail.com>
2013-05-29 13:40:49 +0800

    开发中 初步功能第二版

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -14,6 +14,7 @@
                     success: function(data) {
                         me.currentData = data;
                         me.rendTbody(data);
+                        me.rendSelect(data);
                     }
                  });
             },
@@ -21,9 +22,9 @@
             bindEvent: function () {
                 var me = this;
                 //点击一键下拉中的选择按钮
-                 $("#onKeyControll .dropdown-menu").on("click", "li", function(e) {
+                 $("#oneKeyBlock .dropdown-menu").on("click", "li", function(e) {
                      e.preventDefault();
-                     //交互data-type值和文字
+                     //交换data-type值和文字
                      var nowKey = $("#oneKey"),
                          newKey = $(this).children(),
                          nowKeyText = nowKey.html(),
@@ -34,6 +35,14 @@
                      nowKey.attr("data-type", newKeyType);
                      newKey.html(nowKeyText);
                      newKey.attr("data-type", nowKeyType);
+                     //交换引起的联动
+                     if(newKeyType == 1) {
+                         $("#payerBlock>span").html("付款人：");
+                         $("#oneKeyBlock>span").html("付款总额(¥)：");
+                     } else {
+                         $("#payerBlock>span").html("充值人：");
+                         $("#oneKeyBlock>span").html("充值额度(¥)：");
+                     }
                      
                      //清空输入框
                      $("#cash").val('');
@@ -48,21 +57,30 @@
                     if(me.isClicked){
                         return;
                     }
+                    if(!me.isRightValue()) {
+                        $("#inputAlert").removeClass("hidden");
+                        $("#inputAlert").show();
+                        return false;
+                    };
+                    $("#inputAlert").hide();
                     me.isClicked = true;
                     var oldText = $(this).html();
                     $(this).addClass("disabled");
                     $(this).next().addClass("disabled");
                     $(this).html("loading...");
-                    
                     //拼提交的参数
                     var type = $(this).attr("data-type"),
                         id = me.getCheckedValue(),
                         cash = $("#cash").val(),
                         remain = me.getCheckedRemain();
+                        payerId = me.getParamByName("id", $("#payer").val());
+                        payerRemain = me.getParamByName("remain", $("#payer").val());
                         query = "type=" + type +
-                                "&id=" + id +
+                                "&idArr=" + id +
                                 "&cash=" + cash +
-                                "&remain=" + remain;
+                                "&remainArr=" + remain +
+                                "&payerId=" + payerId + 
+                                "&payerRemain=" + payerRemain;
                     console.log(query);
                     $.ajax({
                         type:'POST',
@@ -79,6 +97,14 @@
                         }
                     });
                 });
+            },
+            
+            isRightValue: function() {
+                var val = $("#cash").val();
+                if(/\D/.test(val)) {
+                    return false;
+                }
+                return true;
             },
             
             getCheckedValue: function() {
@@ -104,13 +130,28 @@
                 return remainArr.join(",");
             },
             
-            changeTableType: function(type) {
+            getParamByName: function(key, name) {
+                var me = this,
+                    param = null;
+                $.each(me.currentData.result, function(index, item){
+                    if(item.name === name) {
+                        param = item[key];
+                        return;
+                    }
+                });
+                return param;
+            },
+            
+            changeTableType: function(type, payerId) {
                 if(type == 1) {
                     this.setAllChecked(true);
                 } else {
                     this.setAllChecked(false);
                 }
-               
+                if(payerId) {
+                    var index = parseInt(payerId) - 1;
+                    $("#table tbody tr").eq(index).addClass("info");
+                }
             },
             //表格中多选框全选或者全不选
             setAllChecked: function(status) {
@@ -123,17 +164,31 @@
                  }
             },
             
+            rendSelect: function(data) {
+               var options = '';
+               $.each(data.result, function(index, item) {
+                   options += '<option>' + item.name + '</option>';
+               });
+               $("#payer").html(options);
+            },
+            
             rendTbody: function(data) {
                 var me = this,
                     itemArr = data.result,
-                    tbody = '';
+                    tbody = '',
+                    tfoot = '';
                 $.each(itemArr, function(index, item) {
                     item.time = me.parseTimeStamp(item.time);
                     tbody += me.getRowHtml(item);
                 });
+                if (data.footResult) {
+                    data.footResult.time = me.parseTimeStamp(data.footResult.time);
+                }
+                tfoot = me.getFootHtml(data.footResult);
                 $("#table tbody").html(tbody);
+                $("#table tfoot").html(tfoot);
                 //改变table的显示状态
-                me.changeTableType(data.message.type);
+                me.changeTableType(data.message.type, data.message.payerId);
             },
             
             parseTimeStamp: function(timeStamp) {
@@ -158,6 +213,7 @@
             },
             
             getRowHtml: function(item) {
+                if(!item) {return;}
                 var row = 
                     '<tr>'+
                       '<td>'+
@@ -171,7 +227,20 @@
                       '<td>' + item.time + '</td>' +
                    '</tr>';
                 return row;
+            },
+            
+            getFootHtml: function(item) {
+                if(!item) {return;}
+                 var row = 
+                    '<tr class="success">'+
+                      '<td>' + item.name + '</td>' +
+                      '<td>' + item.consume + '</td>' +
+                      '<td>' + item.remain + '</td>' +
+                      '<td>' + item.time + '</td>' +
+                   '</tr>';
+                return row;
             }
+            
         };
         
         Client.init();
--- routes/index.js
+++ routes/index.js
@@ -7,10 +7,70 @@ var obj = {
     success: true,
     message: {
         type: 1,
+        payerId: null,
         error: ''
     },
-    result: []
+    result: [],
+    footResult: {
+      // "name": "总额",
+      // "consume": "-50",
+      // "remain": 9253,
+      // "time": "1369665569102"
+    }
 };
+function getFormatResult(result) {
+    var data = [];
+    result.forEach(function(item, index) {
+        if(item.consume > 0) {
+            item.consume = "+" + item.consume;
+        }
+        data.push(item);
+    });
+    return data;
+}
+
+function getFootResult(result, payerId, type) {
+    var foot = {
+        name: "总额",
+        consume: 0,
+        remain: 0,
+        time: new Date().getTime()
+    };
+    result.forEach(function(item, index) {
+        var consume = parseFloat(item.consume),
+            remain = parseFloat(item.remain);
+            time = item.time;
+        foot.consume += consume;
+        foot.remain += remain;
+        foot.time = time;
+    });
+    
+    if(foot.consume > 0) {
+        foot.consume = "+" + foot.consume;
+    }
+    
+    if(payerId && type === 2) {
+        var index = getIndexFromListById(result, payerId);
+        foot.consume = "+" + parseFloat(result[index].consume);
+        foot.time = result[index].time;
+    }
+    if(/\./.test(foot.remain.toString())){
+        foot.remain = foot.remain.toFixed(1);
+    }
+    return foot;
+}
+
+function getIndexFromListById(list, id) {
+    var me = this,
+        index = null;
+   list.forEach(function(item, i){
+        if(item.id === id) {
+            index = i;
+            return;
+        }
+    });
+    return index;
+}
 
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
@@ -19,53 +79,77 @@ exports.index = function(req, res){
 exports.get = function(req, res){
     var query = req.body.id ? ("id=" + req.body.id) : null;
     Finnace.get(query, function(err, data) {
-        obj.result = data;
+        obj.result = getFormatResult(data);
+        obj.footResult = getFootResult(data);
+       
         res.send(obj);
     })
 };
 
 exports.post = function(req, res){
-    var id = req.body.id,
-        type = req.body.type,
-        cash = req.body.cash || 0,
-        remain = req.body.remain;
-    var idArr = id ? id.split(",") : [],
-        remainArr = remain ? remain.split(",") : [],
-        len = idArr.length;
-    idArr.forEach(function(id, index) {
-        cash = parseInt(cash);
-        id = parseInt(id);
-        remain = parseInt(remainArr[index]);
-        
+    var idArr = req.body.idArr,
+        remainArr = req.body.remainArr,
+        type =        parseInt(req.body.type),
+        payerId =     parseInt(req.body.payerId),
+        cash =        parseFloat(req.body.cash) || 0,
+        payerRemain = parseFloat(req.body.payerRemain);
+    idArr = idArr ? idArr.split(",") : [];
+    remainArr = remainArr ? remainArr.split(",") : [];
+     var len = idArr.length;
+    //充值模式
+    var options = {
+        'id' : payerId,
+        'consume': cash,
+        'remain':  payerRemain + cash,
+        'time': new Date().getTime()
+    };
+    Finnace.update(options, function(err) {
+        if(err) {
+        }
+        remainArr[payerId - 1] = options.remain;
+        console.log(payerId + "充值成功");
         //消费模式
-        var consume = parseFloat((cash/len).toFixed(1));
-        var options = {
-            'id' : id,
-            'consume': "-" + consume,
-            'remain':  remain-consume,
-            'time': new Date().getTime()
-        };
-        //充值模式
-        if (type == 2) {
-            options.consume = "+" + cash;
-            options.remain = remain + cash;
+        if(type === 1) {
+            idArr.forEach(function(id, index) {
+                var id = parseInt(id);
+                var remain = parseInt(remainArr[index]);
+                
+                var consume = parseFloat((cash/len).toFixed(1));
+                var options = {
+                    'id' : id,
+                    'consume': -consume,
+                    'remain':  remain-consume,
+                    'time': new Date().getTime()
+                };
+                Finnace.update(options, function(err) {
+                    if(err) {
+                    }
+                    console.log(id + "扣款成功");
+                })
+            });
         }
         
-        Finnace.update(options, function(err) {
-            if(err) {
+        //返回列表数据
+        //FIXME 这里没有deferred，是sql按照顺序执行的缘故，然后保证了回调执行的顺序？
+        Finnace.get(null, function(err, data) {
+            console.log("数据返回成功");
+            obj.result = getFormatResult(data);
+            obj.footResult = getFootResult(data, payerId, type);
+            obj.message.type = type;
+            obj.message.payerId = payerId;
+            
+            if(type === 1) {
+                //充值人在返回数据中的顺序
+                var payerIndex = getIndexFromListById(data, payerId);
+                var payerConsume = data[payerIndex].consume;
+                //对充值人的显示做双显示处理
+                obj.result[payerIndex].consume = "+" + cash + "/" + payerConsume;
+                //对总额做双显示处理
+                obj.footResult.consume = "+" + cash + "/" + obj.footResult.consume;
             }
-            console.log(id + "更新成功");
-        })
-    });
-    
-    //FIXME 这里没有deferred，是sql按照顺序执行的缘故，然后保证了回调执行的顺序？
-    Finnace.get(null, function(err, data) {
-        console.log("数据返回成功");
-        obj.result = data;
-        obj.message.type = type;
-        res.send(obj);
+            res.send(obj);
+        });
     })
-    
 };
 
 
